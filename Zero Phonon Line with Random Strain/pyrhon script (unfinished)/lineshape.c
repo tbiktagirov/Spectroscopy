70d6fb91ddb991008692b164024389df48310740
tbiktagirov <tbiktagirov@gmail.com>
2016-06-05 19:48:40 +0300

    Update lineshape.c

--- Zero Phonon Line with Random Strain/pyrhon script (unfinished)/lineshape.c
+++ Zero Phonon Line with Random Strain/pyrhon script (unfinished)/lineshape.c
@@ -10,16 +10,17 @@ double h(int n, double args[n]){
     double x3 = (1-args[2])/args[2];
     double x4 = (1-args[3])/args[3];
     double freq = args[4];
-    double G = args[5];
+    double delta = args[5];
     double gamma = args[6];
     double v1 = args[7];
     double v2 = args[8];
-	
-    //double J = 1/((1-args[0])*(1-args[1])*(1-args[2])*(1-args[3])*(1-args[0])*(1-args[1])*(1-args[2])*(1-args[3]));
+    double xc = args[9];
+    
+
     double J = 1/(args[0]*args[1]*args[2]*args[3]*args[0]*args[1]*args[2]*args[3]);
-    double delta = sqrt((x1+x2) * (x1+x2) + (x3+x4) * (x3+x4));
-    double L1 = exp(-(freq - 1190.1917 + delta)*(freq - 1190.1917 + delta)/2/(G*G));
-    double L2 = exp(-(freq - 1190.1917 - delta)*(freq - 1190.1917 - delta)/2/(G*G));
+    double E = sqrt((x1+x2) * (x1+x2) + (x3+x4) * (x3+x4));
+    double L1 = exp(-(freq - xc + E)*(freq - xc + E)/2/(delta*delta));
+    double L2 = exp(-(freq - xc - E)*(freq - xc - E)/2/(delta*delta));
     double distr = pow(1/(x1*x1/v1/v1+x2*x2/v2/v2+x3*x3/v1/v1+x4*x4/v2/v2+gamma*gamma),2.5);
     double h = (L1+L2)*distr*J;
     return h;
